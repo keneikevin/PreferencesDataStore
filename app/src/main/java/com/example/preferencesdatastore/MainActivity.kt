b2a13a52bada0322ff6c5dc8ce804b487f0f01be
kenei <kevinkenei9@gmail.com>
2021-12-27 21:39:57 +0300

    Preference DataStore

--- app/src/main/java/com/example/preferencesdatastore/MainActivity.kt
+++ app/src/main/java/com/example/preferencesdatastore/MainActivity.kt
@@ -19,18 +19,79 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-   
+    //datastore object
+    private lateinit var dataStore: DataStore<Preferences>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //
+        dataStore = createDataStore(name = "settings")
 
+        binding.btnRead.setOnClickListener {
+            lifecycleScope.launch {
+              val value = read(binding.etReadkey.text.toString())
+                binding.tvReadValue.text = value ?: "No Value Found"
+            }
+        }
 
+        binding.btnSave.setOnClickListener {
+            lifecycleScope.launch {
+                save(
+                    binding.etSaveKey.text.toString(),
+                    binding.etSaveValue.text.toString()
+                )
+            }
+        }
     }
 
+//save into data store
+    private suspend fun save(key:String, value:String){
+        val dataStoreKey = preferencesKey<String>(key)
+    dataStore.edit { settings->
+        settings[dataStoreKey] = value
+    }
+    }
+
+
+    //save into data store
+    private suspend fun read(key:String):String?{
+        val dataStoreKey = preferencesKey<String>(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
